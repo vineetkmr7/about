a2bd46334ab75d897875b6027f51541fbd34f917
Vineet Kumar <vineetkumar@outlook.in>
2020-08-27 01:07:09 +0530

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'about';
+  loadingDots: string = '';
+  interval;
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      if (this.loadingDots.length === 3) {
+        this.loadingDots = '';
+      } else {
+        this.loadingDots += '.';
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.interval);
+  }
 }
